27d3f19abe902ab14647913010613e80db0cb149
rajasekaran.s <proffraja@gmail.com>
2020-03-10 15:31:50 +0530

    first commit

---
+++ src/main/java/com/kgisl/raja/App.java
@@ -0,0 +1,49 @@
+package com.kgisl.raja;
+import com.kgisl.raja.model.Department;
+import com.kgisl.raja.model.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!");
+        StandardServiceRegistry standardServiceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(standardServiceRegistry).getMetadataBuilder().build();
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        Student student = new Student();
+
+        student.setId(70);
+        student.setName("Mandd");
+        student.setAge(23);
+
+        session.merge(student);
+
+        transaction.commit();
+
+        transaction = session.beginTransaction();
+
+        Department department = new Department();
+        department.setId(400);
+        department.setName("ECE");
+        department.setStartyear(2008);
+        session.persist(department);
+        transaction.commit();
+        
+        System.out.println("Data Saved");
+        session.close();
+        sessionFactory.close();
+    }
+}
---
+++ src/main/java/com/kgisl/raja/model/Department.java
@@ -0,0 +1,33 @@
+package com.kgisl.raja.model;
+
+public class Department {
+    private Integer id;
+    private String name;
+    private Integer startyear;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Integer getStartyear() {
+        return this.startyear;
+    }
+
+    public void setStartyear(Integer startyear) {
+        this.startyear = startyear;
+    }
+
+
+}
